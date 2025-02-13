961f97fbbb60490e7485fb080614096d1b1cfb19
maamokun/MikanDev <maamo-kun@outlook.com>
2025-02-13 18:34:25 +0900

    init

---
+++ main.ts
@@ -0,0 +1,18 @@
+import { Elysia } from 'elysia';
+import fs from 'node:fs';
+
+const blacklist = fs.readFileSync('./blacklist.txt', 'utf-8').split('\n');
+
+const app = new Elysia()
+
+app.get('/', (req) => {
+  const url = req.query.url
+    if (!url) return new Response('No URL provided', { status: 400 });
+    if (blacklist.includes(url)) return new Response('URL is blacklisted', { status: 403 });
+    return new Response('URL is not blacklisted', { status: 200 });
+});
+
+app.listen(process.env.PORT || 3000);
+
+console.log(`Listening on ${app.server!.url}`);
+
