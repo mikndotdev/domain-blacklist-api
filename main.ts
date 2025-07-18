0d5142e5dc4414cade7d19cf2d7eeb6b8d38dd6c
maamokun/MikanDev <sachadeco214@gmail.com>
2025-07-18 11:34:50 +0900

    add: excluded domain

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import { Elysia } from 'elysia';
 import fs from 'node:fs';
 
 const blacklist = fs.readFileSync('./blacklist.txt', 'utf-8').split('\n');
+const exclusions = fs.readFileSync('./exclusions.txt', 'utf-8').split('\n')
 
 const app = new Elysia()
 
@@ -9,7 +10,9 @@ app.get('/', (req) => {
   const url = req.query.url
     if (!url) return new Response('No URL provided', { status: 400 });
     const domain = new URL(url).hostname;
-    if (blacklist.includes(domain)) return new Response('URL is blacklisted', { status: 403 });
+    if (blacklist.includes(domain) && !exclusions.includes(domain)) {
+      return new Response('URL is blacklisted', { status: 403 });
+    }
     return new Response('URL is not blacklisted', { status: 200 });
 });
 
